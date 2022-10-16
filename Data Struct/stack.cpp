b0141c16fcb5853e8f2f4b69681ff41ffbe63e60
Lyccrius <lyccrius@outlook.com>
2022-10-16 14:28:00 +0800

    update 栈

--- Data Struct/stack.cpp
+++ Data Struct/stack.cpp
@@ -4,20 +4,28 @@ struct Stack {
     int head;
 
     Stack() {
-        head = -1;
+        head = 0;
     }
 
     T top() {
-        return array[head];
+        return array[head - 1];
     }
 
     void push(T other) {
-        array[++head] = other;
+        array[head--] = other;
         return;
     }
 
     void pop() {
-        --head;
+        head--;
         return;
+    }
+
+    bool empty() {
+        return head;
+    }
+
+    int size() {
+        return head;
     }
 }
