ee904193075a7c608d2d4c53d30ab555a55b366f
Lyccrius <lyccrius@outlook.com>
2022-10-18 12:34:58 +0800

    add hash.cpp

---
+++ Data Struct/hash.cpp
@@ -0,0 +1,32 @@
+struct HashMap {
+    struct Edge {
+        long long key;
+        int value;
+        int nexx;
+    };
+
+    Edge data[size << 1];
+    int head[size];
+    int cnt;
+
+    HashMap() {
+        cnt = 0;
+        memset(head, 0, sizeof(head));
+    }
+
+    int hash(long long key) {
+        return key % size;
+    }
+
+    int& operator [](long long key) {
+        int h = hash(key);
+        for (int i = head[h]; i; i = data[i].next) {
+            if (data[i].key == key) {
+                return data[i].value;
+            }
+        }
+        data[++cnt] = (Edge){key, -1, head[h]};
+        head[h] = cnt;
+        return data[cnt].value;
+    }
+};
