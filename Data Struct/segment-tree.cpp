a9a6d90024b0481b4d93fde7c2b8769af46ca4d9
Lyccrius <lyccrius@outlook.com>
2022-11-05 17:14:40 +0800

    update data structure segment-tree

--- Data Struct/segment-tree.cpp
+++ Data Struct/segment-tree.cpp
@@ -1,24 +1,89 @@
 #include <cstdio>
 
-struct SegmentTree {
-    int array[maxn];
-    int node[maxn << 2];
+const int maxn = 1e5 + 10;
 
-    void Build(int l, int r, int xn) {
-    //  xn is a leaf node
+struct SegmentTree {
+    struct Node {
+        int l, r;
+        int lNode, rNode;
+        int tag;
+        int val;
+    }
+
+    int array[maxn];
+    Node node[maxn << 2];
+
+    void Build(int l, int r, int xNode) {
+    //  set l and r of xNode
+        node[xNode].l = l;
+        node[xNode].r = r;
+
+    //  xNode is a leaf node
         if (l == r) {
-            node[x] = array[l];
-        //  node[x] = array[r];
+            node[xNode].val = array[l];
+        //  node[xNode].val = array[r];
             return;
         }
 
-    //  xn is not a leaf node
-        int mid = (l + r) >> 1;
-        Build(l, mid, x << 1);
-        Build(mid + 1, r, x << 1 + 1);
-        node[x] = node[x << 1] + node[x << 1 + 1];
+    //  xNode is not a leaf node
+        int mid = l + r >> 1;
+    //  int mid = l + (r - l >> 1);
+        node[xNode].lSon = xNode << 1;
+        node[xNode].rSon = xNode << 1 + 1;
+        Build(l, mid, node[xNode].lSon);
+        Build(mid + 1, r, node[xNode].rSon);
+        node[xNode].val = node[node[xNode].lSon].val
+                        + node[node[xNode].rSon].val;
+
         return;
     }
 
-    
+    void PushDown(int xNode) {
+        int lNode = node[xNode].lNode;
+        int rNode = node[xNode].rNode;
+        node[lNode].tag += node[xNode].tag;
+        node[rNode].tag += node[xNode].tag;
+        node[lNode].val += node[xNode].tag * (node[lNode].r - node[lNode].l + 1);
+        node[rNode].val += node[xNode].tag * (node[rNode].r - node[rNode].l + 1);
+        node[xNode].tag = 0;
+        return;
+    }
+
+    void PushUp() {
+
+    }
+
+    void Modify(int l, int r, int xNode, int xVal) {
+    //  [l, r] completely contains xNode
+        if (node[xNode].l >= l && 
+            node[xNode].r <= r) {
+            node[xNode].tag += xVal;
+            node[xNode].val += xVal * (r - l + 1);
+            return;
+        }
+
+    //  [l, r] incompletely contains xNode
+        if (node[xNode].tag && 
+            node[xNode].l != node[xNode].r) PushDown(xNode);
+        int lNode = node[xNode].lNode;
+        int rNode = node[xNode].rNode;
+        if (l <= node[lNode].r) Modify(l, r, lNode, xVal);
+        if (r >= node[rNode].l) Modify(l, r, rNode, xVal);
+        return;
+    }
+
+    int Query(int l, int r, int xNode) {
+    //  [l, r] completely contains xNode
+        if (l <= node[xNode].l && 
+            r >= node[xNode].r) return node[xNode].val;
+
+    //  [l, r] imcompletely contain xNode
+        int lNode = node[xNode].lSon;
+        int rNode = node[xNode].rSon;
+        if (node[xNode].tag) PushDown(xNode);
+        int sum = 0;
+        if (l <= node[lNode].r) sum += Query(l, r, lNode);
+        if (r >= node[rNode].l) sum += Query(l, r, rNode);
+        return sum;
+    }
 };
