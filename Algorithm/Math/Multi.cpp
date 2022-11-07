f441abce71ed5a53109bb392147ab6a059c053a2
Lyccrius <lyccrius@outlook.com>
2022-11-07 16:38:55 +0800

    update

---
+++ Algorithm/Math/Multi.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+long long multi(long long a, long long b, long long p) {
+    long long ans = 1;
+    while (b) {
+        if (b & 1) ans = (ans * a) % p;
+        b >>= 1;
+        a = (a * 2) % p;
+    }
+    return ans;
+}
---
+++ Algorithm/Math/Pow.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+
+long long pow(long long a, long long b, long long p) {
+    long long ans = 0;
+    while (b) {
+        if (b & 1) ans = (ans + a) % p;
+        b >>= 1;
+        a = (a * a) % p;
+    }
+    return ans;
+}
